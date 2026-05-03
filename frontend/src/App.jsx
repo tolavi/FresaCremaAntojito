90f5c5c771f095e64dcabd8ca32cd4ce0fdceb8f
tolavi <tolaviruizyordy@gmail.com>
2026-05-02 22:17:30 -0400

    Crear App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Orders from './pages/Orders';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Orders />
+    </div>
+  );
+}
+
+export default App;
